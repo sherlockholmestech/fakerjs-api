ea8b8b42f06a7589aee910eb1584a2bfe1194167
Sherlock Holmes <sherlockholmesxp@gmail.com>
2023-01-27 21:52:20 +0800

    init

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-const express = require('express')
-const app = express()
-const port = 8001
+const express = require('express');
+const { faker } = require('@faker-js/faker');
+
+const app = express();
+const port = 8001;
 
 app.get('/words/10', (req, res) => {
-  res.send('Hello World!')
+  res.send(faker.random.words(10));
 })
 
+app.get('/words/30', (req, res) => {
+	res.send(faker.random.words(30));
+  })
+
+app.get('/words/50', (req, res) => {
+	res.send(faker.random.words(50));
+  })
+
+app.get('/words/100', (req, res) => {
+	res.send(faker.random.words(100));
+  })
+
+app.get('/words/200', (req, res) => {
+	res.send(faker.random.words(200));
+  })
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
